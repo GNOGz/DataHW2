1aef5e1e7920225b43d0a00f8687693841bb2523
GNOGz <99389733+GNOGz@users.noreply.github.com>
2024-07-11 00:40:42 +0700

    print structure + pushback

--- hw2/DynamicArray.java
+++ hw2/DynamicArray.java
@@ -1,53 +1,80 @@
+
 public class DynamicArray {
+
     private int[] arr;
     private int capacity;
-    private int size; // Last element can be indexed at size-1
-    
-    public DynamicArray(int cap){ // Class Constructor
+    private int size = 0; // Last element can be indexed at size-1
+
+    public DynamicArray(int cap) { // Class Constructor
         arr = new int[cap];
         capacity = cap;
     }
-    
-    public void PushBack(int data){
+
+    public void PushBack(int data) {
         // START FIX CODE HERE
+        if (size < capacity) {
+            arr[size] = data;
+            size++;
+        } else {
+            int[] temp = new int[size];
+
+            for (int i = 0; i < size; i++) {
+                temp[i] = arr[i];
+            }
+
+            arr = new int[size * 2];
+
+            for (int i = 0; i < size; i++) {
+                arr[i] = temp[i];
+            }
+            capacity*=2;
+            arr[size] = data;
+            size++;
+        }
         // END CODE HERE
     }
-    public int PopBack(){
+
+    public int PopBack() {
         // START FIX CODE HERE
         return 0;
         // END CODE HERE
     }
 
-    public int Get(int i){
+    public int Get(int i) {
         // START FIX CODE HERE
         return 0;
         // END CODE HERE
     }
-    public void Set(int i, int value){
+
+    public void Set(int i, int value) {
         // START FIX CODE HERE
         // END CODE HERE
     }
-    
-    public void Remove(int i){
+
+    public void Remove(int i) {
         // START FIX CODE HERE
         // END CODE HERE
     }
-    
-    public boolean IsEmpty(){
+
+    public boolean IsEmpty() {
         // START FIX CODE HERE
         return false;
         // END CODE HERE
     }
-    
-    public int GetSize(){
+
+    public int GetSize() {
         // START FIX CODE HERE
         return 0;
         // END CODE HERE
     }
-    
-    public void PrintStructure(){
+
+    public void PrintStructure() {
         // START FIX CODE HERE
-        System.out.print("Size = " + size + ", Cap = " + capacity + ", arr = [ ] ");
+        System.out.print("Size = " + size + ", Cap = " + capacity + ", arr = [ ");
+        for (int i = 0;i<size;i++){
+            System.out.print(arr[i]+", ");
+        }
+        System.out.println("]");
         // END CODE HERE
     }
 }
--- hw2/Main.java
+++ hw2/Main.java
@@ -3,6 +3,16 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         DynamicArray var1 = new DynamicArray(2);
+        //test below
+        var1.PushBack(3);
+        var1.PushBack(4);
+        var1.PrintStructure();
+        var1.PushBack(5);
+        var1.PrintStructure();
+        var1.PushBack(5);
+        var1.PushBack(8);
+        var1.PushBack(20);
+        var1.PrintStructure();
     }
     
 }
